ec76a64dd7e1017280a26aa90686930463a52cc7
taiga1071101 <taigatukimi@gmail.com>
2025-02-23 19:21:45 +0900

    犬の画像表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-
+  let [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/poodle-miniature/n02113712_5675.jpg");
   return (
     <>
-      <button onClick={() => console.log('押した')}>ボタン</button>
-      <p>ああ</p>
+      <div>
+        <p>犬の画像サイト！</p>
+        <button onClick={() => {
+          fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then((data) => {
+            setDogUrl(data.message);
+          });
+        }}>更新</button>
+      </div>
+      <div>
+        <img src={dogUrl} ></img>
+      </div>
     </>
   )
 }
